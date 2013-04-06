c3ab7bf9c5a3b96af9b1d66792522285c50926b6
Peter Metcalf <peter@peter-P5K.(none)>
2013-04-06 12:33:49 +0100

    Add Project Programale Timer

---
+++ ProgramableTimer/gen/com/possemeeg/android/programabletimer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.possemeeg.android.programabletimer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
